afbdc3addc3c8ff0131564b3eb1979eedeb468ae
sergi <ssergi.alesanco.perich@gmail.com>
2022-11-07 20:57:22 +0100

    add 3x03

---
+++ Ex03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char lletra;
+            do
+            {
+                Console.WriteLine("Escriu s o n");
+                lletra = Console.ReadKey().KeyChar;
+            }
+
+            while (lletra == 's');
+        }
+    }
+}
