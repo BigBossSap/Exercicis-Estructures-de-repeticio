b32495281317f8d7d354f68494b0df48c00ffc65
sergi <ssergi.alesanco.perich@gmail.com>
2022-11-07 20:53:08 +0100

    ex02

---
+++ Ex02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*2.Programa que validi una nota(0..10) amb dowhile*/
+
+            double nota;
+
+            do
+            {
+
+                Console.WriteLine("Escribe una nota: ");
+                nota = Convert.ToDouble(Console.ReadLine());
+
+            }
+
+            while (nota < 0 || nota > 10);
+            
+
+
+            Console.WriteLine("Nota aceptada!");
+
+
+        }
+    }
+}
