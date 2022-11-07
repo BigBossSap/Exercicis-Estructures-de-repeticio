835ca75593dc5f3a86272b131cf0e9d53e0e7402
sergi <ssergi.alesanco.perich@gmail.com>
2022-11-07 20:47:17 +0100

    Agregar archivos de proyecto.

---
+++ Ex01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 1. Programa que validi una nota (0..10) amb while. Que demani una nota, i si no
+   està entre 0 i 10 que la torni a demanar, i si no està ....
+   */
+            double nota;
+            nota = Convert.ToDouble(Console.ReadLine());
+            while (nota < 0 || nota > 10)
+            {
+
+                Console.WriteLine("Nota incorrecta!");
+                nota = Convert.ToDouble(Console.ReadLine());
+            }
+            
+
+            Console.WriteLine("Nota aceptada!");
+
+
+
+
+        }
+    }
+}
