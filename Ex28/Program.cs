594afac56f816c1a24ee0122c2395833373457d8
Sergi Alesanco Perich <sergi.alesanco.perich@gmail.com>
2022-11-15 12:13:33 +0100

    add 29

---
+++ Ex28/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ex28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*28. Fer un programa que digui si un número és apilable: Ho són el 0, 1, 3, 6, 10, 15,
+   21, ...
+   */
+            int i = 0,num, suma=0;
+            Console.WriteLine("num:");
+            num = int.Parse(Console.ReadLine());
+
+
+           while (i <= num)
+            {
+
+                suma += i;
+                i++;
+
+                if (suma == num)
+                    Console.WriteLine("SI");           
+
+
+            }
+
+            
+            
+
+            
+
+        }
+    }
+}
---
+++ Ex29/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Ex29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*28. Fer un programa que digui si un número és apilable: Ho són el 0, 1, 3, 6, 10, 15,
+   21, ...
+   */
+            int i = 0, num, suma = 0, j = 1, aplicable = 0;
+           
+
+
+            while (j <= 1000)
+            {
+
+                while (i <= j)
+                {
+
+                    suma += i;
+                    i++;
+
+                    if (suma == j)
+                    {
+                        
+                        aplicable += suma;
+                        Console.WriteLine(aplicable);
+
+                    }
+
+                    
+
+                }
+                j++;
+                
+
+
+            }
+        }
+
+
+    }
+}   
