736cb6992856601a15f2266cd1ff9ba4f21749a2
Sergi Alesanco Perich <sergi.alesanco.perich@gmail.com>
2022-11-16 12:55:29 +0100

    add 35

---
+++ Ex35/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Ex35
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*2. Demanar un número i mostrar-lo en binari
+   */
+
+            int num, resto,divisio=0;
+            string binari = "";
+
+            Console.WriteLine("num: ");
+            num = int.Parse(Console.ReadLine());
+
+            while (num!=1)
+            {
+
+                resto = num % 2;
+                divisio = num / 2;
+
+                num = divisio;
+
+                binari = resto + binari; 
+                
+                
+                
+                
+
+            }
+
+            binari = divisio + binari;
+            Console.WriteLine(binari);
+
+
+        }
+    }
+}
